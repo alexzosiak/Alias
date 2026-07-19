b782b4baa11f32b40358f264756b8d1434dca464
AlekseyZosyak <110170863+AlekseyZosyak@users.noreply.github.com>
2026-07-19 14:13:00 +0100

    fix build and settings styles import

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"nFdq6LY28vi20a8H7heZM4qqVSk33tWfDZS0RxqLzT0=\"\n}"
--- .next/dev/static/chunks/app_layout_tsx_007e4b2._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_next_dist_1iml2--._.js","static/chunks/_19aunow._.css"],
-    source: "dynamic"
-});
