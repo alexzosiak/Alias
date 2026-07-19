8e71b7f26a4ecaf55d9eb2baf68a4ae6fd1f17c0
AlekseyZosyak <110170863+AlekseyZosyak@users.noreply.github.com>
2026-07-19 13:54:06 +0100

    fons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,18 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono, Ribeye } from 'next/font/google';
+// import { Geist, Geist_Mono } from 'next/font/google';
 import {  } from '@/components/game/Background/Background';
 import '@/styles/globals.scss';
 import { Background } from '@/components/game/Background/Background';
 
-const geistSans = Geist({
-    variable: '--font-geist-sans',
-    subsets: ['latin'],
-});
+// const geistSans = Geist({
+//     variable: '--font-geist-sans',
+//     subsets: ['latin'],
+// });
 
-const geistMono = Geist_Mono({
-    variable: '--font-geist-mono',
-    subsets: ['latin'],
-});
+// const geistMono = Geist_Mono({
+//     variable: '--font-geist-mono',
+//     subsets: ['latin'],
+// });
 
 export const metadata: Metadata = {
     title: 'Create Next App',
@@ -27,7 +27,7 @@ export default function RootLayout({
     return (
         <html
             lang="en"
-            className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+            // className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
         >
             <body className="min-h-full flex flex-col">
                 <Background/>
